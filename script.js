8f2b12d86615d9f0acb3551036147a147deb545c
Edita <editapociene@gmail.com>
2022-05-24 10:15:31 +0300

    second task done

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ block.style.height = '300px';
 block.style.margin = 'auto';
 block.style.textAlign = 'center';
 block.style.fontSize = '24px';
+
+var antrasDivas = document.createElement('div');
+antrasDivas.setAttribute('id', 'moku2');
+antrasDivas.innerHTML = 'Valio pavyko!';
+antrasDivas.style.width = '300px';
+antrasDivas.style.height = '300px';
+antrasDivas.style.margin = 'auto';
+antrasDivas.style.textAlign = 'center';
+antrasDivas.style.fontSize = '24px';
+antrasDivas.style.backgroundColor = '#f1c40f';
+
+document.body.appendChild(antrasDivas);
+// console.log(antrasDivas);
+
+var button = document.createElement('button');
+button.innerHTML = 'SPAUDALIUKAS';
+button.setAttribute('id', 'spaudaliukas');
+document.body.appendChild(button);
+button.style.display = 'block';
+button.style.margin = '5px auto';
+
+button.addEventListener('click', function() {
+    this.style.display = 'none';
+    antrasDivas.style.display = 'none';
+});
