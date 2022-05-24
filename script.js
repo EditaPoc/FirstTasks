4da111b1c4712a88ecfbaf3a13493d12747f50fb
Edita <editapociene@gmail.com>
2022-05-24 09:46:19 +0300

    first task of tasks done

---
+++ script.js
@@ -0,0 +1,7 @@
+var block = document.getElementById('moku');
+block.style.backgroundColor = '#2ecc71';
+block.style.width = '300px';
+block.style.height = '300px';
+block.style.margin = 'auto';
+block.style.textAlign = 'center';
+block.style.fontSize = '24px';
